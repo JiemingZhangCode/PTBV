be87ced98635b7dc5aa072ab805d4f21c4eae5a6
JiemingZhangCode <163628303+JiemingZhangCode@users.noreply.github.com>
2024-07-25 19:05:41 +0800

    Update change.m

--- code/change.m
+++ code/change.m
@@ -1,7 +1,7 @@
 function [img_TVB,img_en] = change(img)
-%   img  输入选取的帧图像
-%   img_TVB 输出区块tvb值的合成图
-%   img_en  输出图像区块低频占比的合成图
+%   img  
+%   img_TVB 
+%   img_en  
 % graypic = img(: , : , 1)*1.20-img(: , : , 2)*0.90-img(: ,: , 3)*0.90;
 graypic=rgb2gray(img);
 graypic = uint8(graypic);
@@ -10,8 +10,8 @@ row = round(row/16) * 16;
 col = round(col/16) * 16;
 graypic = imresize(graypic, [row, col]);
 
-img_TVB = zeros(row, col); % 存放转换后的TVB系数
-img_en = zeros(row, col); % 存放转换后的en系数
+img_TVB = zeros(row, col); 
+img_en = zeros(row, col); 
 for m=1:16:row-15
     for n=1:16:col-15
         img_block = graypic(m:m+15, n:n+15);
@@ -22,14 +22,14 @@ for m=1:16:row-15
         %b=sum(a(:,3).^2)/10000;
         b=sum((a(:,1)./255).^2.*a(:,3))/100;
         
-        f=double(img_block);%数据类型转换
-        g=fft2(f);%图像傅里叶转换
-        g=fftshift(g);%傅里叶变换平移
-        [N1,N2]=size(g);%傅里叶变换图像尺寸
+        f=double(img_block);
+        g=fft2(f);
+        g=fftshift(g);
+        [N1,N2]=size(g);
         d0=5;
-        n1=fix(N1/2);%数据圆整
-        n2=fix(N2/2);%数据圆整
-        for i=1:N1%遍历图像像素
+        n1=fix(N1/2);
+        n2=fix(N2/2);
+        for i=1:N1
             for j=1:N2
                 d=sqrt((i-n1)^2+(j-n2)^2);
                 if d==0
@@ -37,7 +37,7 @@ for m=1:16:row-15
                 else
                     h=1/(1+(d/d0)^(2*2));
                 end
-                result(i,j)=h*g(i,j);%?图像矩阵计算处理
+                result(i,j)=h*g(i,j);
             end
         end
         result=ifftshift(result);

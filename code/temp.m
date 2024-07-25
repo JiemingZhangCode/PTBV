d80091bff3b82382f1535be486f6ffe94ad298d8
JiemingZhangCode <163628303+JiemingZhangCode@users.noreply.github.com>
2024-07-25 19:38:34 +0800

    Update temp.m

--- code/temp.m
+++ code/temp.m
@@ -1,18 +1,18 @@
 clear;clc;
 f=fullfile('2333.mp4');
-obj=VideoReader(f);%读取视频文件
-numFrames=obj.NumFrames;%计算总帧数
-number = 1;%标号
+obj=VideoReader(f);
+numFrames=obj.NumFrames;
+number = 1;
 
-img=read(obj,1);%读取第1帧图片
+img=read(obj,1);
 [imgTVB,imgen]=smoke(img);
 
 TVBS=[];
 Ens=[];
 
-for k=1:60:numFrames %按固定间隔抽取图片（我这里每帧读取一次）
-    img=read(obj,k);%读取第k帧图片
+for k=1:60:numFrames 
+    img=read(obj,k);
     adressString = ['C:\Users\lenovo\Desktop\3\' ,sprintf('%0.4d', number),'.jpg'];
     imwrite(img, adressString,'jpg');
-    number = number + 1;%标号加一
+    number = number + 1;
 end

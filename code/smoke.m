f1b85f63fb0073ed23c96e5adf903748e5f7e9bb
JiemingZhangCode <163628303+JiemingZhangCode@users.noreply.github.com>
2024-07-25 19:38:07 +0800

    Update smoke.m

--- code/smoke.m
+++ code/smoke.m
@@ -8,14 +8,14 @@ a=tabulate(graypic(:));
         %b=sum(a(:,3).^2)/10000;
 b=sum((a(:,1)./255).^2.*a(:,3))/100;
         
-f=double(graypic);%数据类型转换
-g=fft2(f);%图像傅里叶转换
-g=fftshift(g);%傅里叶变换平移
-[N1,N2]=size(g);%傅里叶变换图像尺寸
+f=double(graypic);
+g=fft2(f);
+g=fftshift(g);
+[N1,N2]=size(g);
 d0=5;
-n1=fix(N1/2);%数据圆整
-n2=fix(N2/2);%数据圆整
-for i=1:N1%遍历图像像素
+n1=fix(N1/2);
+n2=fix(N2/2);
+for i=1:N1
     for j=1:N2
         d=sqrt((i-n1)^2+(j-n2)^2);
         if d==0
@@ -23,7 +23,7 @@ for i=1:N1%遍历图像像素
         else
             h=1/(1+(d/d0)^(2*2));
         end
-        result(i,j)=h*g(i,j);%?图像矩阵计算处理
+        result(i,j)=h*g(i,j);
     end
 end
 result=ifftshift(result);
